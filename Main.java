5e5b31e378db9fcdd6e7ba5503c458501dcb5200
Ivan Smetskii <smetskii.ivan@gmail.com>
2021-03-11 03:04:30 +0300

    123

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientCurrentBalance = 100;
+        int depositValue = 5000;
+        int totalBonus;
+        if (depositValue >= 1000) {
+            clientCurrentBalance = 100 + depositValue / 100;
+            totalBonus = depositValue / 100;
+        } else {
+            clientCurrentBalance = 100;
+            totalBonus = 0;
+        }
+        System.out.println("Total bonus: " + totalBonus);
+
+
+    }
+}
